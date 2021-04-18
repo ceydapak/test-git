879b38f17627e76b098d7455a1dba647249d6d19
Ceyda PAK <ceyda12345pak@gmail.com>
2021-04-18 18:13:51 +0300

    second commit

--- src/com/ceyda/test/Test.java
+++ src/com/ceyda/test/Test.java
@@ -3,5 +3,6 @@ package com.ceyda.test;
 public class Test {
     public static void main(String[] args) {
         System.out.printf("github test");
+        System.out.println("hello github, im coming :)");
     }
 }
